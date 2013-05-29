e45d01a5c031fa3e668d9944d480f4fe3452c70c
isma3l <karlo.ismael@gmail.com>
2013-05-29 02:07:51 -0300

    controlador

---
+++ TP2-Promociones/src/com/uba/tecnicas/promo/controlador/Controlador.java
@@ -0,0 +1,68 @@
+package com.uba.tecnicas.promo.controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import com.uba.tecnicas.promo.domain.Caja;
+import com.uba.tecnicas.promo.domain.Producto;
+import com.uba.tecnicas.promo.domain.exception.MarketException;
+
+
+
+public class Controlador {
+
+	private Caja caja;
+	
+	public Controlador(Caja caja) {
+		this.caja = caja;
+	}
+
+	// iniciar venta
+	private class botonIniciarVenta implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			try {
+				caja.iniciarVenta();
+			} catch (MarketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+	}
+	
+	public ActionListener getListenerBotonIniciarVenta() {		
+		return new botonIniciarVenta();
+	}
+	
+	// abrir caja
+	private class botonAbrirCaja implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			caja.abrir(Calendar.getInstance());	
+		}
+	}
+	
+	public ActionListener getListenerBotonAbrirCaja() {		
+		return new botonAbrirCaja();
+	}
+	
+	// cerrar caja
+	private class botonCerrarCaja implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			caja.cerrar();
+		}
+	}
+	
+	public ActionListener getListenerBotonCerrarCaja() {		
+		return new botonCerrarCaja();
+	}
+	
+	public void agregarProducto(Producto producto, int cantidad) {
+		caja.agregarProducto(producto, cantidad);
+	}
+	
+	
+	//public void finalizarVenta(List<Oferta> ofertas, FormaPago forma) {
+		// caja.finalizarVenta(List<Oferta> ofertas, FormaPago forma);
+	//}
+	
+
+}
---
+++ TP2-Promociones/src/com/uba/tecnicas/promo/vista/StreamOutput.java
@@ -0,0 +1,21 @@
+package com.uba.tecnicas.promo.vista;
+
+import java.io.OutputStream;
+import java.io.IOException;
+import javax.swing.JTextArea;
+
+public class StreamOutput extends OutputStream{
+
+	   private JTextArea textArea;
+	     
+	    public StreamOutput(JTextArea textArea) {
+	        this.textArea = textArea;
+	    }
+	     
+	    @Override
+	    public void write(int b) throws IOException {
+	        // redirige los datos al textArea
+	        textArea.append(String.valueOf((char)b));
+	        textArea.setCaretPosition(textArea.getDocument().getLength());
+	    }	
+}
