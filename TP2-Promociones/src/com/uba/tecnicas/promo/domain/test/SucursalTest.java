a486aa47f07c1327e262d10214804a23bb72497a
Nestor <Nestor@Nestor-PC>
2013-06-02 17:37:03 -0300

    Test de "Existe promo Coca+Sprite 30% descuento en la Sprite"

--- TP2-Promociones/src/com/uba/tecnicas/promo/domain/test/SucursalTest.java
+++ TP2-Promociones/src/com/uba/tecnicas/promo/domain/test/SucursalTest.java
@@ -3,6 +3,7 @@ package com.uba.tecnicas.promo.domain.test;
 import static org.junit.Assert.*;
 
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.SortedMap;
 import java.util.Vector;
 
@@ -42,9 +43,9 @@ public class SucursalTest {
 	@Before
 	public void setUp() throws Exception {
 		sucursal = new Sucursal();
-		coca = new Producto("CocaCola", 6, "Bebidas");
-		sprite = new Producto("Sprite", 7, "Bebidas");
-		maceta = new Producto("Maceta", 5, "Jardineria");
+		coca = new Producto("CocaCola", 1, "Bebidas");
+		sprite = new Producto("Sprite", 1, "Bebidas");
+		maceta = new Producto("Maceta", 10, "Jardineria");
 		
 		condicion1 = new CondicionSobreUnItem(new ItemComprado(coca, 1));
 		condicion2 = new CondicionSobreUnItem(new ItemComprado(sprite, 1));
@@ -53,8 +54,10 @@ public class SucursalTest {
 		condicionCompuesta.agregar(condicion2);
 		descuento = new DescuentoSobreProducto(sprite, 0.3);
 		oferta = new Oferta("coca+sprite 30%", condicion1, descuento, true);
-		caja1 = new Caja(new Vector<Oferta>(), sucursal);
-		caja2 = new Caja(new Vector<Oferta>(), sucursal);
+		Collection<Oferta> ofertas = new Vector<Oferta>();
+		ofertas.add(oferta);
+		caja1 = new Caja(ofertas, sucursal);
+		caja2 = new Caja(ofertas, sucursal);
 	}
 	
 	@Test
@@ -75,20 +78,35 @@ public class SucursalTest {
 		caja1.finalizarVenta(FormaPago.CREDITO);
 		caja2.finalizarVenta(FormaPago.EFECTIVO);
 		SortedMap<String, Integer> rank = sucursal.getRanking();
-		
-		
 		assertEquals(new Integer(6), rank.get("Sprite"));
 		assertEquals(new Integer(5), rank.get("Maceta"));
 		assertEquals(new Integer(4), rank.get("CocaCola"));
-		
-		
 		assertEquals(new Integer(6), rank.get(rank.firstKey()));
 		rank.remove(rank.firstKey());
 		assertEquals(new Integer(5), rank.get(rank.firstKey()));
 		rank.remove(rank.firstKey());
 		assertEquals(new Integer(4), rank.get(rank.firstKey()));
-		
 	}
 	
+	@Test
+	public void testCocaMasSpite30Descuento() throws MarketException {
+		caja1.abrir(Calendar.getInstance());		
+		caja1.iniciarVenta();
+		caja1.agregarProducto(coca, 3);
+		caja1.agregarProducto(sprite, 2);
+		caja1.agregarProducto(maceta, 1);
+		caja1.finalizarVenta(FormaPago.CREDITO);
+		assertEquals(14.4, caja1.getVenta().getTotal(), 0.05);
+	}
 	
+	@Test
+	public void testOtraVentaCocaMasSpite30Descuento() throws MarketException {
+		caja1.abrir(Calendar.getInstance());		
+		caja1.iniciarVenta();
+		caja1.agregarProducto(coca, 1);
+		caja1.agregarProducto(sprite, 4);
+		caja1.agregarProducto(maceta, 4);
+		caja1.finalizarVenta(FormaPago.CREDITO);
+		assertEquals(44.7, caja1.getVenta().getTotal(), 0.05);
+	}
 }
